a867216c3e2eacce7fb1921cf210ed0fa28e9a92
Aaron Jordan <aaron@impactfulmedia.co>
2025-03-10 21:23:04 -0500

    fix up relation

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,6 @@
 class Item < ApplicationRecord
   has_rich_text :desc
-  has_many :item_categories
+  has_many :item_categories, dependent: :destroy
   has_many :categories, through: :item_categories
   has_one_attached :cover_image
   has_many_attached :images
--- app/models/item_category.rb
+++ app/models/item_category.rb
@@ -1,4 +1,4 @@
 class ItemCategory < ApplicationRecord
-  belongs_to :item, dependent: :destroy
-  belongs_to :category, dependent: :destroy
+  belongs_to :item
+  belongs_to :category
 end
